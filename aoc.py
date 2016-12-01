bcd430c90f4f080b2f16e0459a17b39e74264e70
Erik Kinding <erikkinding@gmail.com>
2016-12-01 09:37:39 +0100

    initial

---
+++ aoc.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from aoc2016.code import day1
+
+day1 = day1.Day1()
+
+day1.day1_1()
---
+++ aoc2016/code/day1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class Day1:
+
+    def __init__(self):
+        self.lol = 1
+
+    def day1_1(self):
+        print('trololol')
+
