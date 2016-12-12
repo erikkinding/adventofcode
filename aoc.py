13cd7a58f1927b5615d2d8050271c34813bbf8a1
Erik Kinding <erikkinding@gmail.com>
2016-12-12 08:17:24 +0100

    day 12 code

--- aoc.py
+++ aoc.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from aoc2016.code import day11
+from aoc2016.code import day12
 from misc import gol
 
 
 def main():
 
-    day11.Day11().part1()
+    day12.Day12().part2()
     #day8.Day8('clear', True).part1()
 
     #gol.Gol().run()
---
+++ aoc2016/code/day12.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+
+class Day12:
+
+    def __init__(self):
+        self.registers = {}
+
+    # Answer: 318077
+    def part1(self):
+        self.registers = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
+        self.run_instructions()
+
+    # Answer: 9227731
+    def part2(self):
+        self.registers = {'a': 0, 'b': 0, 'c': 1, 'd': 0}
+        self.run_instructions()
+
+    def run_instructions(self):
+        data = open('aoc2016/input/day12.txt', 'r').read().splitlines()
+
+        idx = 0
+        while idx < len(data):
+            split = data[idx].split()
+
+            if split[0] == 'cpy':
+                value = self.get_value(split[1])
+                self.registers[split[2]] = value
+
+            if split[0] == 'inc':
+                self.registers[split[1]] += 1
+
+            if split[0] == 'dec':
+                self.registers[split[1]] -= 1
+
+            if split[0] == 'jnz':
+                value = self.get_value(split[1])
+                if value != 0:
+                    idx += self.get_value(split[2])
+                    continue
+
+            idx += 1
+
+        print(self.registers)
+
+    def get_value(self, value):
+        value = value
+        try:
+            value = int(value)
+        except:
+            value = self.registers[value]
+            pass
+        return value
