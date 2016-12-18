68cf3e9545f7869588ddbcb0f54e8b7bfa82dd82
erik kinding <erikkinding@gmail.com>
2016-12-18 17:47:48 +0100

    day 11 code

--- aoc.py
+++ aoc.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from aoc2016 import day11_alt
+from aoc2016 import day11_alt2
 from aoc2016 import day15
 from aoc2016 import day16
 from aoc2016 import day17
@@ -8,9 +8,9 @@ from aoc2016 import day18
 
 def main():
 
-    #day11_alt.Day11_alt().part1()
+    day11_alt2.Day11().part1()
     #day16.Day16().part2()
-    day18.Day18().part2()
+    #day18.Day18().part2()
 
 if __name__ == "__main__":
     main()
