689e462f3d89ac250ddd751d91c2b7edf51c6838
Erik Kinding <erikkinding@gmail.com>
2016-12-20 16:26:53 +0100

    day 20 part 2 code added

--- aoc2016/day20.py
+++ aoc2016/day20.py
@@ -6,15 +6,11 @@ class Day20:
     def __init__(self):
         pass
 
-    # Answer: 4793564
+    # Answer part 1: 4793564
+    # Answer part 2: 146
     def part1(self):
         t0 = time.time()
         self.solve()
-        print('Elapsed: ' + str(time.time() - t0) + 's')
-
-    def part2(self):
-        t0 = time.time()
-        self.solve2()
         print('Elapsed: ' + str(time.time() - t0) + 's')
 
     def solve(self):
@@ -28,53 +24,23 @@ class Day20:
         sorted_ranges = sorted(ranges)
 
         i = 0
-        lowest = 2147483647
+        ips = 0
+        lowest = 4294967294 # unsigned integer max value
 
-        while i <= 2147483647:
+        while i <= 4294967294:
             in_range = False
             for r in sorted_ranges:
                 if i in range(r[0], r[1]+1):
                     in_range = True
+                    i = r[1]
                     break
 
             if not in_range:
-                lowest = i
-                print('New lowest: ' + str(i))
-                break
+                if i < lowest:
+                    lowest = i
+                ips += 1
 
             i += 1
 
         print('Lowest: ' + str(lowest))
-
-
-    # part 2: 73 too low...
-    def solve2(self):
-
-        data = open('aoc2016/input/day20.txt', 'r').read().splitlines()
-
-        ranges = []
-        for row in data:
-            split = row.split('-')
-            ranges.append( [int(split[0]), int(split[1])])
-
-        sorted_ranges = sorted(ranges)
-        print(sorted_ranges)
-        stretched_ranges = [sorted_ranges[0]]
-
-        for r in sorted_ranges:
-            for sr in stretched_ranges:
-                # if hit, stretch range
-                if r[0] in range(sr[0], sr[1]+1):
-                    sr[1] = r[1]
-                # no hit, add new range
-                else:
-                    stretched_ranges.append(r)
-
-        print(stretched_ranges)
-
-        print('N ranges: ' + str(len(sorted_ranges)))
-        print('S ranges: ' + str(len(stretched_ranges)))
-
-        allowed = []
-        print('N IPs: ' + str(len(allowed)))
-        pass
+        print('N IPs: ' + str(ips))
