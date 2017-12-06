b69456a85153e3b6b5caa8e769048bdfe9ea34f5
Erik Kinding <erik.kinding@greenbyte.com>
2017-12-06 11:39:54 +0100

    tidying up day six

--- aoc2017/six.py
+++ aoc2017/six.py
@@ -3,14 +3,9 @@
 inp = [5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6]
 #inp = [0, 2, 7, 0]
 inp_len = len(inp)
+memory = inp
 
-def first_index_of(value, memory):
-    for idx, mem_value in enumerate(memory):
-        if mem_value == value:
-            return idx
-
-
-def redistribute_memory(index, memory):
+def redistribute_memory(index):
     value = memory[index]
     memory[index] = 0
 
@@ -19,56 +14,43 @@ def redistribute_memory(index, memory):
         memory[index % inp_len] += 1
         value -= 1
 
-    return memory
 
-
-def mem_hash(memory):
+def mem_hash():
     return '_'.join(map(str, memory))
 
 
 def part_one():
+    
+    state_cache = {}
+    state_cache[mem_hash()] = 1
+
+    reallocations = 0
+    while True:
+        mem_max_index = memory.index(max(memory))
+        redistribute_memory(mem_max_index)
+        reallocations += 1
+
+        new_mem_hash = mem_hash()
+        if new_mem_hash in state_cache:
+            break
+        else:
+            state_cache[new_mem_hash] = 1
+
+    return reallocations
+
+
+def part_two():
+    # reset state if part_one was run before:
+    inp_len = len(inp)
     memory = inp
-    state_cache = {}
-    state_cache[mem_hash(memory)] = 1
-
-    reallocations = 0
-    while True:
-        mem_max_index = first_index_of(max(memory), memory)
-        memory = redistribute_memory(mem_max_index, memory)
-        reallocations += 1
-
-        new_mem_hash = mem_hash(memory)
-        if new_mem_hash in state_cache:
-            break
-        else:
-            state_cache[new_mem_hash] = 1
-
-    print("Part1 reallocations: ", reallocations)
-    return memory
-
-
-def part_two(memory):
-    state_cache = {}
-    state_cache[mem_hash(memory)] = 1
-
-    reallocations = 0
-    while True:
-        mem_max_index = first_index_of(max(memory), memory)
-        memory = redistribute_memory(mem_max_index, memory)
-        reallocations += 1
-
-        new_mem_hash = mem_hash(memory)
-        if new_mem_hash in state_cache:
-            break
-        else:
-            state_cache[new_mem_hash] = 1
-
-    print("Part2 loop size: ", reallocations)
+    
+    part_one()
+    return part_one()
 
 
 def main():
-    looping_state = part_one()
-    part_two(looping_state)
+    print("Reallocations: ", part_one())
+    print("Loop size: ", part_two())
 
 
 if __name__ == "__main__":
