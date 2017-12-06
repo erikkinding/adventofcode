c9cbc7a10a92421693b99f1ea7151e0e6ef094d1
Erik Kinding <erikkinding@gmail.com>
2017-12-06 22:22:30 +0100

    some utility stuff

---
+++ aoc2017/aoc.py
@@ -0,0 +1,30 @@
+ # -*- coding: utf-8 -*-
+
+def get_file(filename):
+    return open("input/" + filename, 'r')
+
+
+def input_as_rows(filename):
+    return get_file(filename).read().splitlines()
+
+
+def input_as_values(filename, t):
+    return map(t, get_file(filename).read().split())
+
+
+def input_as_grid(filename, t):
+    rows = input_as_rows(filename)
+    grid = []
+    for row in rows:
+        grid.append(map(t, row.split()))
+
+    return grid
+
+
+def input_as_grid(filename):
+    rows = input_as_rows(filename)
+    grid = []
+    for row in rows:
+        grid.append(row.split())
+
+    return grid
