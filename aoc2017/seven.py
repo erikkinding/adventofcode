18e0b05b317859a3d1790b30f82ab3f173f44f5a
Erik Kinding <erikkinding@gmail.com>
2017-12-08 07:27:17 +0100

    ...

--- aoc2017/seven.py
+++ aoc2017/seven.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from collections import Counter
 import aoc
-inp = aoc.input_as_rows("seven.txt")
+inp = aoc.input_as_rows("seven_test.txt")
 
 # node? (name, weight, connections)
 def print_stuff(stuff):
@@ -20,7 +20,6 @@ def setup_programs():
         connections = []
         if len(rs) > 1:
             connections = list(map(lambda x : (x.strip(), []), rs[1].split(", ")))
-            shit.extend(sc)
             tree.append((name, connections))
         
         # weight data outside of tree
@@ -52,12 +51,77 @@ def part1():
     counts = Counter(programs)
     print(counts)
 
+
+#def find_rec(tree, target):
+#    return find_rec(tree, target, 0)
+
+def find_mod_rec(tree, target, replacement, ret):
+    for node in tree:
+        if any(node[1]):
+            find_mod_rec(node[1], target, replacement, ret)
+
+        #print(node)
+        if node[0] == target:
+            print(node)
+            print(replacement)
+            print("HIT!")
+            node = replacement
+
+
+def find_rec(tree, target, depth):
+    for node in tree:
+        #print(node)
+        if any(node[1]):
+            find_rec(node[1], target, depth+1)
+
+        if node[0] == target:
+            print("Hit taget at depth ", depth)
+
+    return
+
+
+def print_tree_rec(tree, depth):
+    for node in tree:
+        offset = "--" * depth
+        print("|" + offset + str(node[0]))
+        if any(node[1]):
+            print_tree_rec(node[1], depth+1)
+
+    return
+
+
+sums = {}
 def part2():
+    tree = setup_programs()
+
+    for node in tree:
+        key = ""
+        ksum = 0
+        for c in node[1]:
+            key += c[0]
+            ksum += int(programs[c[0]]) + int(programs[node[0]])
+        sums[key] = ksum
+
+    print(sums.items())
+
+
+    print_stuff(tree)
+    print_stuff(programs.items())
+
+def part3():
+
+    tree = [('root', [ ('b1', [ ('c1', []), ('c2', []), ('c3', [('d1', []), ('d2', [])]), ('c4', [])]), ('b2', [])])]
+
+    print_tree_rec(tree, 1)
+    #find_rec(tree, 'c4', 0)
+    tree = find_mod_rec(tree, 'd1', ('d1', [('e1', [])]), [])
+    print_tree_rec(tree, 1)
+
     print("part2")
 
 def main():
     #print(inp)
-    part1()
+    #part1()
     part2()
 
 if __name__ == "__main__":
