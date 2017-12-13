72a04ecf2a87a6cd7f442dcabb523b520e93f9d2
Erik Kinding <erikkinding@gmail.com>
2017-12-13 07:45:21 +0100

    thirteen part one

---
+++ aoc2017/thirteen.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+
+from collections import defaultdict
+import aoc
+
+#inp = aoc.input_as_grid("ex.txt", int)
+inp = aoc.input_as_rows("thirteen.txt")
+inp = aoc.input_as_rows("thirteen.txt")
+
+
+# starting at zero, find current position of scanner i a given depth
+# at a given time. Depth start at 0
+def scanner_position(pics, srange):
+    step = 1
+    position = 0
+    for i in range(pics):
+        position += step
+
+        if position == srange - 1:
+            step = -1
+        if position == 0:
+            step = 1
+
+    return position
+
+
+# 632
+def part1():
+
+    # firewall configuration
+    firewall = defaultdict(int)
+    last_depth = 0
+    for row in inp:
+        s = row.split(': ')
+        depth = int(s[0])
+        srange = int(s[1])
+        firewall[depth] = srange
+        last_depth = depth
+
+    severity = 0
+    position = 0
+    pics = 0
+    # go through
+    while position <= last_depth:
+
+        # validate state
+        scanner_range = firewall[position]
+        scanner_pos = scanner_position(pics, scanner_range)
+        if scanner_pos == 0:
+            severity += scanner_range * position
+
+        # move me
+        position += 1
+
+        # update pics
+        pics += 1
+
+
+    print("part1 severity:", severity)
+
+def part2():
+    # firewall configuration
+    firewall = defaultdict(int)
+    last_depth = 0
+    for row in inp:
+        s = row.split(': ')
+        depth = int(s[0])
+        srange = int(s[1])
+        firewall[depth] = srange
+        last_depth = depth
+
+
+    min_pics = 0;
+    while True:
+        position = 0
+        pics = min_pics
+        # go through
+        hit = False
+        while position <= last_depth:
+
+            # validate state
+            scanner_range = firewall[position]
+            scanner_pos = scanner_position(pics, scanner_range)
+            if scanner_range > 0 and scanner_pos == 0:
+                hit = True
+                break
+
+            # move me
+            position += 1
+
+            # update pics
+            pics += 1
+
+
+        if not hit:
+            break
+        min_pics += 1
+
+    print("part2 min pics:", min_pics)
+
+def main():
+    part1()
+    part2()
+
+if __name__ == "__main__":
+    main()
